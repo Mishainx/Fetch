cbe7e9f668adf2a3ea192c3451afd66dab358534
mishainx <fer.r@live.com.ar>
2023-12-20 14:06:46 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default async function Home() {
 
   const response = await fetch( `https://${process.env.VERCEL_URL}/api/products`)
   const data = response.json()
+  console.log(process.env.VERCEL_URL)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
