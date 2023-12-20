e5511df255fa562ef2e9e58c3fb5985158d1b440
mishainx <fer.r@live.com.ar>
2023-12-20 16:38:14 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
 
 export default async function Home() {
 
-  try {
     const response = await fetch(`https://fetch-ten-rho.vercel.app//api/products`);
     const data = await response.json()
 
-  } catch (error) {
-    console.error('An error occurred while fetching the data:', error);
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
