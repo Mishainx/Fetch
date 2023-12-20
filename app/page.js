05716458d26f290f2a95c1f553d66330c81a8ece
mishainx <fer.r@live.com.ar>
2023-12-20 17:04:47 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 
-  const fetch = await fetch( `https://${NEXT_PUBLIC_VERCEL_URL}/api/products` )
-  const data = await fetch.json()
 
 
 
 export default async function Home() {
 
+  const fetch = await fetch( `https://${NEXT_PUBLIC_VERCEL_URL}/api/products` )
+  const data = await fetch.json()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
