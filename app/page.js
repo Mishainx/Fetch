359d6cfb803e77210dae16fc431ab1d1e87b564c
mishainx <fer.r@live.com.ar>
2023-12-20 16:17:34 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -1,19 +1,8 @@
 
 export default async function Home() {
 
-  try {
     const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`);
     const data = response.json()
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-    } else {
-      console.error(`Error: ${response.status} - ${response.statusText}`);
-    }
-  } catch (error) {
-    console.error('An error occurred while fetching the data:', error);
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
