a819113fbb5f8a92b63062764126249d0c0aa9ec
mishainx <fer.r@live.com.ar>
2023-12-20 18:12:04 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
 export default async function Home() {
   let data
   try{
-  const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products `)
-  data = await response.json()
-  }
-  catch(error){
-    console.log(error.message)
-  }
+    const [data, setData] = useState(null);
 
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`);
+          const jsonData = await response.json();
+          setData(jsonData);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+  
+      fetchData();
+    }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div>{data}</div>
