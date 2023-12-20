5d333ff34b7086c76737a7b76cdb05838d91f19e
mishainx <fer.r@live.com.ar>
2023-12-20 15:32:03 -0300

    modificaiones

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -4,7 +4,7 @@ export const GET = async (request,{params}) =>{
 
 
     try{
-            return NextResponse.json("macaco", { status: 200 });
+            return NextResponse.json("probando", { status: 200 });
     }
     catch(error){
         return NextResponse.json("Error al obtener producto.", { status: 500 })
