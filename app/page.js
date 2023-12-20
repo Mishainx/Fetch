0462fc10f3c30454dccccdf5170bab2d9c12cc60
mishainx <fer.r@live.com.ar>
2023-12-20 14:10:23 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 
 export default async function Home() {
 
-  const response = await fetch( `https://${process.env.VERCEL_URL}/api/products`)
+  const response = await fetch( `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`)
   const data = response.json()
   console.log(process.env.VERCEL_URL)
 
