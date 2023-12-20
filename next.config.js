7273ee8251f64897e757a5cdfdee94bb19a35c25
mishainx <fer.r@live.com.ar>
2023-12-20 14:49:01 -0300

    modificaciones

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-          {
-            // Apertura total CORS para entorno de desarrollo
-            source:  ` ${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-            headers: [
-              {
-                key: 'Access-Control-Allow-Origin',
-                value: '*',
-              },
-            ],
-          },
-        ];
-      },
+
 }
 
 module.exports = nextConfig
