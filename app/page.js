ed13b6e075e9d421c977da87f1066208f0e34a23
mishainx <fer.r@live.com.ar>
2023-12-20 16:44:23 -0300

    modificaciones

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default async function Home() {
     const data = await response.json()
 
     console.log(`https://fetch-ten-rho.vercel.app/api/products`)
-    console.log(process.env.VERCEL_URL)
+    console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
